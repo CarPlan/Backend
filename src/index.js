166cfead61c3803b0ded99c55a6a62518f463069
Kauruck <adrian.kumbrink@gmx.de>
2021-08-26 09:08:46 +0200

    Added basic express app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+const express = require('express');
+const app = express();
+app.use(express.json({limit:'1mb'}));
+const port = 3001;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/api', (req, res) => {
+    console.log(req.body);
+    res.json({
+        status:'succsess'
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
