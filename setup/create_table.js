0b94d7582be2f6fe1585dc5ff1303a3f63c800ef
Adrian Kumbrink <adrian.kumbrink@gmx.de>
2021-09-05 16:55:48 +0200

    [Setup] Removed init of tokens table

--- setup/create_table.js
+++ setup/create_table.js
@@ -4,16 +4,10 @@ const fs = require('fs')
 
 async function createTables(){
     await createUserTable();
-    await createTokensTable();
 }
 
 async function createUserTable(){
     const query = fs.readFileSync("./setup/create_table_user.sql").toString();
-    await pool.query(query);
-}
-
-async function createTokensTable(){
-    const query = fs.readFileSync("./setup/create_table_tokens.sql").toString();
     await pool.query(query);
 }
 
